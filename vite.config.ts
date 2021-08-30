c0338e2c026f8aee9f6499586259e349473b059a
mskuroedov <mskuroedov@gmail.com>
2021-08-30 22:14:37 +0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://mskuroedov.github.io/react-sandbox',
   plugins: [reactRefresh()]
 })
